dadcaf30dbe79c0a67ee244503d63618963a9f6c
Henok8515 <henokegezew44@gmail.com>
2021-11-24 22:42:06 +0300

    updating

---
+++ models/postMessage.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+const postSchema = mongoose.Schema({
+    title: String,
+    email:String,
+    message: String,
+    creator: String,
+    tags: [String],
+    selectedFile: String,
+ 
+    createdAt: {
+        type: Date,
+        default: new Date(),
+    },
+})
+
+var PostMessage = mongoose.model('PostMessage', postSchema);
+
+export default PostMessage;
